91de2ccc27b92c16cecccfb0b1b9d7cd40f2d3e3
YanTonghui <603627730@qq.com>
2023-03-23 21:05:26 +0800

    33

--- src/main/java/com/example/hututest/config/MyInterceptor.java
+++ src/main/java/com/example/hututest/config/MyInterceptor.java
@@ -14,6 +14,9 @@ public class MyInterceptor implements HandlerInterceptor {
         System.out.println("11");
         System.out.println("m2");
         System.out.println("m1");
+        System.out.println("m1");
+        System.out.println("m1");
+
 //        11
 //        fff
     }
