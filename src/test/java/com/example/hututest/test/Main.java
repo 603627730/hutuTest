ce265a54d43f3aedbeacaf878d2f3d7cf4a7d9a6
YanTonghui <603627730@qq.com>
2023-03-27 09:11:41 +0800

    333

--- src/test/java/com/example/hututest/test/Main.java
+++ src/test/java/com/example/hututest/test/Main.java
@@ -2,7 +2,9 @@ package com.example.hututest.test;
 
 import org.apache.catalina.AsyncDispatcher;
 import org.apache.commons.compress.utils.Lists;
+import org.junit.platform.commons.util.StringUtils;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
@@ -10,6 +12,8 @@ import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aa" + null);
+        String s = "";
+        System.out.println(s.length());
+
     }
 }
