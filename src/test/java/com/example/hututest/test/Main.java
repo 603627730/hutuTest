b2ce53bd81e8c2719959e815bef9212778d4a03e
YanTonghui <603627730@qq.com>
2023-03-31 09:31:30 +0800

    测试11

--- src/test/java/com/example/hututest/test/Main.java
+++ src/test/java/com/example/hututest/test/Main.java
@@ -1,17 +1,11 @@
 package com.example.hututest.test;
 
-import org.apache.catalina.AsyncDispatcher;
-import org.apache.commons.compress.utils.Lists;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "***33";
-        String ss = "1*33";
-        System.out.println(s.substring(Math.max(s.length() - 4, 0)));
+        for (int i = 0; i < 12; i++) {
+            System.out.println(1);
+        }
     }
+
 }
