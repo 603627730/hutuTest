c6c1c475a3cd9a587cb0226fd93bfaf1337f7231
YanTonghui <603627730@qq.com>
2023-04-17 09:08:31 +0800

    1

--- src/test/java/com/example/hututest/test/Main.java
+++ src/test/java/com/example/hututest/test/Main.java
@@ -1,21 +1,18 @@
 package com.example.hututest.test;
 
-import org.apache.catalina.AsyncDispatcher;
-import org.apache.commons.compress.utils.Lists;
-import org.junit.platform.commons.util.StringUtils;
+import com.example.hututest.config.Payment;
+import com.example.hututest.io.UserController;
 
-import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "32133333";
-        s = s.replaceAll("(\\d{4}).*(\\d{4})", "$1****$2");
-        System.out.println(s);
-
+        Optional<Payment> payment = Optional.empty();
+        if (payment.isPresent()){
+            System.out.println(payment.get());
+        }
+        Payment payment1 = payment.orElse(new Payment());
+        System.out.println(payment1);
 
     }
 }
